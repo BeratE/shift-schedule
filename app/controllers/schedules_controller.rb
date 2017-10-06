d5fdf242774e7321b24a39f9b7470815223ef2c3
Berat Ertural <Beertural@gmail.com>
2017-10-06 19:25:31 +0200

    Added Schedule Table, Views, etc.

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -9,10 +9,20 @@ class SchedulesController < ApplicationController
     if params[:schedule_date].present? then
       @curr_date = params[:schedule_date].to_time
     end
-    @schedules = Schedule.where(year: @curr_date.year, week: @curr_date.strftime("%V").to_i).to_a
+    @schedules = Schedule.where(year: @curr_date.year, week: @curr_date.strftime("%V").to_i)
+    @versions = Version.all
     @users = User.joins("INNER JOIN user_preferences
                          ON user_preferences.user_id = users.id
                          AND user_preferences.shift_hours > 0").select(:id, :type, :firstname, :lastname, :shift_hours)
+
+  end
+
+  def create
+
+  end
+
+  def new
+
   end
 
   def edit
--- assets/javascripts/ui.js
+++ assets/javascripts/ui.js
@@ -1,3 +1,3 @@
-var datepickerOptions={dateFormat: 'yy-mm-dd', firstDay: 1, showOn: 'button', buttonImageOnly: true,
+var datepickerOptions={dateFormat: 'yy-mm-dd', firstDay: 1, showOn: 'button', buttonImageOnly: false,
   buttonImage: '/images/calendar.png', showButtonPanel: false, showWeek: true, showOtherMonths: true,
   selectOtherMonths: true, changeMonth: true, changeYear: true, beforeShow: beforeShowDatePicker};
