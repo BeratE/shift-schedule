cbcd509c8676b3fbd50a372a0063e40dc2293bea
BeratE <beertural@gmail.com>
2017-10-27 09:56:04 +0200

    fixed crash: creating new users will automatically assign new schedules now

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -9,12 +9,31 @@ class SchedulesController < ApplicationController
       @curr_date = params[:schedule_date].to_time
     end
     session[:curr_date] = @curr_date
+
     #get all schedules and versions for the selected week
     @schedules = Schedule.where(year: @curr_date.year, week: @curr_date.strftime("%V").to_i)
     @versions = Version.joins("JOIN schedules ON schedules.version_id = versions.id
     WHERE schedules.year = #{@curr_date.year} AND schedules.week = #{@curr_date.strftime("%V").to_i}").distinct
     @users = get_users
+
+    #sort all schedules in a 2d hash with [[user.id, version.id]] = hours, if any user is not assigned, new schedules will be created
+    @schedhash = Hash.new
+    @versions.each do |version|
+      @users.each do |user|
+        schedule = @schedules.find_by(user_id: user.id, version_id: version.id)
+        if (schedule.nil?) then #create new schedules
+          @versions.each do |v|
+            Schedule.create(:year => @curr_date.year.to_i, :week => @curr_date.strftime("%V").to_i,
+            :user_id => user.id, :version_id => v.id, :hours => 0)
+            @schedhash[[user.id, v.id]] = 0
+          end
+        else
+          @schedhash[[user.id, version.id]] = schedule.hours
+        end
+      end
+    end
   end
+
 
   #show all versions who havent been added to the selected week for scheduling
   def new
@@ -24,6 +43,7 @@ class SchedulesController < ApplicationController
     WHERE schedules.year = #{@curr_date.year} AND schedules.week = #{@curr_date.strftime("%V").to_i}) s
     ON versions.id = s.version_id WHERE s.id is NULL")
   end
+
 
   #create new schedules with selected version for the week
   def create
@@ -38,10 +58,12 @@ class SchedulesController < ApplicationController
     redirect_to controller: 'schedules', action: 'index', schedule_date: session[:curr_date].strftime('%Y %m %d').gsub!(' ','-')
   end
 
+
   #change hours of all edited schedules
   def edit
 
   end
+
 
 private
   #get all users that have a budget (shift_hours) configured
@@ -50,6 +72,7 @@ private
     ON user_preferences.user_id = users.id
     AND user_preferences.shift_hours > 0").select(:id, :type, :firstname, :lastname, :shift_hours)
   end
+
 
   #validate the given date parameters
   def vali_date (date)
